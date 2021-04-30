e709873a417d0752619a416593e895b65a484551
rioadhii <rio.adhi.pratama11@gmail.com>
2021-04-30 09:41:57 +0700

    add ng-notifier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { MomentPipesModule } from 'ng-moment-pipes';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ForumComponent } from './components/forum/forum.component';
 import { DetailForumComponent } from './components/forum/detail-forum/detail-forum.component';
 
